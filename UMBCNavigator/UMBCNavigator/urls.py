914d8a44ba9584494bfe65a49ec657fac3033302
krisaan <knokuri@gmail.com>
2025-10-26 09:06:55 -0400

    Some syntax changes, add requirements.txt, and update gitignore to ignore cached files

--- UMBCNavigator/UMBCNavigator/urls.py
+++ UMBCNavigator/UMBCNavigator/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from api.views import test_endpoint
 from api.views import get_route
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/test/', test_endpoint),
-    path('api/route/' get_route),
+    path("admin/", admin.site.urls),
+    path("api/test/", test_endpoint),
+    path("api/route/", get_route, name="get_route"),
 ]
--- UMBCNavigator/api/views.py
+++ UMBCNavigator/api/views.py
@@ -8,17 +8,21 @@ from django.conf import settings
 
 ORS_API_KEY = "YOUR_ORS_KEY"
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def get_route(request):
-    start = request.GET.get('start')  # "lon,lat"
-    end = request.GET.get('end')      # "lon,lat"
+    start = request.GET.get("start")  # "lon,lat"
+    end = request.GET.get("end")  # "lon,lat"
 
     url = "https://api.openrouteservice.org/v2/directions/foot-walking"
-    headers = {"Authorization": eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjJmM2NiNzU0ZjQ4NTQxYmJiODNmMTE0OTU4ZTdlODY0IiwiaCI6Im11cm11cjY0In0=}
+    headers = {
+        "Authorization": "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjJmM2NiNzU0ZjQ4NTQxYmJiODNmMTE0OTU4ZTdlODY0IiwiaCI6Im11cm11cjY0In0="
+    }
     params = {"start": start, "end": end}
 
     r = requests.get(url, headers=headers, params=params)
     return Response(r.json())
+
 
 def test_endpoint(request):
     return Response({"message": "Hello from Django!"})
